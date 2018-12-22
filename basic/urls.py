5dee63712a3492db82e3766b77b0f25b51f350c5
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2018-12-22 21:14:03 +0100

    updated files

--- basic/urls.py
+++ basic/urls.py
@@ -16,10 +16,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
+from basic import views
 
 urlpatterns = [
-
+    path('', views.home, name='home'),
     path('project/', include('project.urls')),
     path('fountain/', include('fountain.urls')),
     path('admin/', admin.site.urls), 
