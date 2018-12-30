14f9a63a6a5c731438623acdb0f489d64bf19649
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2018-12-30 19:44:54 +0100

    heroku

--- ecomms/settings/production.py
+++ ecomms/settings/production.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(config('SECRET_KEY'))
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
